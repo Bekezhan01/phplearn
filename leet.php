307e43c3d980d61f92189f0838edaa085480d7eb
Bekezhan Zhumazhan <zhumazhanb@MacBook-Pro-Bekezhan.local>
2024-04-30 20:11:08 +0500

    init

---
+++ leet.php
@@ -0,0 +1,28 @@
+<?php
+//function containsDuplicate($nums): bool
+//{
+//        for ($i = 0; $i <= count($nums); $i++){
+//            for($j = $i + 1; $j <= count($nums) - 1; $j++){
+//                if($nums[$i] === $nums[$j]){
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//}
+//$nums = [1,2,3,4];
+//echo containsDuplicate($nums);
+zettor-hykrip-3qiNho
+ssh-keygen -t ed25519 -C "dedsec011@gmail.com"
+
+function containsDuplicate($nums)
+{
+    $unique = array_unique($nums);
+    if (count($unique) == count($nums)) {
+        return true;
+    }
+    return "0";
+
+}
+$nums = [1,2,3,4,1];
+echo containsDuplicate($nums);
